9830556b504af4cb510a31d02babd661c33051cd
vsalvatelli <vale.salvatelli@gmail.com>
2019-07-08 15:52:53 -0400

    Refactoring

--- src/sdo/io.py
+++ src/sdo/io.py
@@ -5,19 +5,24 @@ from os import path
 from numpy import zeros, load
 from numpy import sqrt
 from scipy.misc import bytescale
+import logging
+from sdo.global_vars import (BASEDIR, FILENAME_TEMPLATE, INITIAL_SIZE, 
+                             B_CHANNELS, UV_CHANNELS)
 
-basedir = '/gpfs/gpfs_gl4_16mb/b9p111/fdl_sw/SDOML'
-filename_template = \
-'{0:s}/{1:04d}/{2:02d}/{3:02d}/{4:s}{5:04d}{6:02d}{7:02d}_{8:02d}{9:02d}_{10:s}.npz'
+AUNIT = 100.0 #units of 100 DN/s/pixel
+BUNIT = 2000.0 #units of 2 kGauss
 
-def sdo_read(year, month, day, hour, min, instr='AIA', channel='0094',
-             subsample=1, basedir=basedir):
+_logger = logging.getLogger(__name__)
+
+
+def sdo_read(year, month, day, hour, minu, instr='AIA', channel='0094',
+             subsample=1, basedir=BASEDIR):
     """
     Purpose: Find an SDOML file, and return image if it exists.
     Parameters:
     year / month / day - a date between May 17 2010 to 12/31/2018
     hour - between 0 and 23
-    min - between 0 and 59 (note AIA data is at 6 min cadence, 
+    minu - between 0 and 59 (note AIA data is at 6 min cadence, 
             HMI at 12 min cadence)
     instr - 'AIA' or 'HMI'
     channel - 
@@ -28,9 +33,9 @@ def sdo_read(year, month, day, hour, min, instr='AIA', channel='0094',
     subsample - return image with every subsample-th pixel in both dimensions
     basedir - directory where the SDO data set is stored.
     
-    Returns: np.array
+    Returns: np.array. Returns -1 if file is not found.
     """
-    file = filename_template.format(basedir,year,month,day,instr,year,
+    file = FILENAME_TEMPLATE.format(basedir,year,month,day,instr,year,
                                     month,day,hour,min,channel)
     if path.isfile(file):
         return ((load(file))['x'])[::subsample,::subsample]
@@ -38,18 +43,18 @@ def sdo_read(year, month, day, hour, min, instr='AIA', channel='0094',
     return -1
 
 
-def sdo_find(year, month, day, hour, min, instrs=['AIA','AIA','HMI'], 
-             channels=['0171','0193','bx'], subsample=1, basedir=basedir, 
+def sdo_find(year, month, day, hour, minu, instrs=['AIA','AIA','HMI'], 
+             channels=['0171','0193','bx'], subsample=1, basedir=BASEDIR, 
              return_images=False):
     
     """
-    Purpose: Find filenames of multiple channels of the SDOML dataset with the same timestamp. 
-    Returns -1 if not all channels are found.
+    Purpose: Find filenames of multiple channels of the SDOML dataset with the same 
+    timestamp. 
 
     Parameters:
     year / month / day - a date between May 17 2010 to 12/31/2018
     hour - between 0 and 23
-    min - between 0 and 59 (note AIA data is at 6 min cadence, HMI at 12 min cadence)
+    minu - between 0 and 59 (note AIA data is at 6 min cadence, HMI at 12 min cadence)
     instr - 'AIA' or 'HMI'
     channel - 
        if instr=='AIA', channel should be one of '0094', '0131', '0171', '0193', '0211',
@@ -58,22 +63,26 @@ def sdo_find(year, month, day, hour, min, instrs=['AIA','AIA','HMI'],
            (last is the line-of-sight component of the magnetic field)
     subsample - return image with every subsample-th pixel in both dimensions
     basedir - directory where the SDO data set is stored.
+    return_images (bool). If False it returns the list of files. If True
+         images are returned.
+    
+    Returns: list of files if return_images False (default). 
+            np.array of shape (n, n, number of channels). 
+            Returns -1 if not all channels are found.
     """
-    from os import path
     files_exist = True
     files = []
-    ind = 0
-    for ch in channels:
-        files.append(filename_template.format(
-            basedir,year,month,day,instrs[ind],year,month,day,hour,min,ch))
+    for ind, ch in enumerate(channels):
+        files.append(FILENAME_TEMPLATE.format(basedir, year, month, day, 
+                                              instrs[ind], year, month, 
+                                              day, hour, minu, ch))
         files_exist = files_exist*path.isfile(files[-1])
-        ind = ind+1
-    print(files)
     if files_exist:
         if (not return_images):
             return files
         else:
-            img = zeros(shape=(int(512/subsample),int(512/subsample),
+            img = zeros(shape=(int(INITIAL_SIZE/subsample), 
+                               int(INITIAL_SIZE/subsample),
                                len(channels)),dtype='float32')
             for c in range(len(channels)):
                 img[:,:,c] = sdo_scale((
@@ -88,71 +97,35 @@ def sdo_bytescale(img, ch):
     Purpose: Given an SDO image of a given channel, returns scaled image
     appropriate for 8-bit display (uint8)
     """
-    aunit = 100.0 #
-    bunit = 2000.0 #units of 2 kGauss  
-
-    if ch == 'bx':
+    if ch in B_CHANNELS:
         return bytescale(img,cmin=-bunit,cmax=bunit)
-    if ch == 'by':
-        return bytescale(img,cmin=-bunit,cmax=bunit)
-    if ch == 'bz':
-        return bytescale(img,cmin=-bunit,cmax=bunit)
-    if ch == '1600':
+    else if ch == UV_CHANNELS:
         return bytescale(sqrt(img),cmin=0,cmax=aunit)
-    if ch == '1700':
-        return bytescale(sqrt(img),cmin=0,cmax=aunit)
-    if ch == '0094':
-        return bytescale(sqrt(img),cmin=0,cmax=aunit)
-    if ch == '0131':
-        return bytescale(sqrt(img),cmin=0,cmax=aunit)
-    if ch == '0171':
-        return bytescale(sqrt(img),cmin=0,cmax=aunit)
-    if ch == '0193':
-        return bytescale(sqrt(img),cmin=0,cmax=aunit)
-    if ch == '0211':
-        return bytescale(sqrt(img),cmin=0,cmax=aunit)
-    if ch == '0304':
-        return bytescale(sqrt(img),cmin=0,cmax=aunit)
-    if ch == '0335':
-        return bytescale(sqrt(img),cmin=0,cmax=aunit)
+    else:
+         _logger.warning("Channel not found, simply bytescaled image is returned")
+        return bytescale(img)
 
-    return bytescale(img)
 
-def sdo_scale(img, ch, inverse=False):
+def sdo_scale(img, ch, aunit=AUNIT, bunit=BUNIT):
     """
-    Purpose: Given an SDO image of a given channel, returned scaled image
-    currently a placeholder. scaling functions are subject to change based on EDA.
+    Purpose: Given an SDO image of a given channel, returned scaled image.
+    Currently a placeholder. scaling functions are subject to change based on EDA.
     """
-    aunit = 100.0 #units of 100 DN/s/pixel
-    bunit = 2000.0 #units of 2 kGauss
-    
-    if inverse:
-        bunit = 1.0/bunit
-        aunit = 1.0/aunit
+   
+    if ch in b_channels:
+        return img/aunit       
+    else if ch in uv_channels:
+        return img/bunit
+    else:
+         _logger.error("Channel not found, input image is returned")
+        return img
 
-    if ch == 'bx':
-        return img/bunit  
-    if ch == 'by':
-        return img/bunit                                                                                                                                              
-    if ch == 'bz':
-        return img/bunit          
-    if ch == '1600':
-        return img/aunit
-    if ch == '1700':
-        return img/aunit
-    if ch == '0094':
-        return img/aunit                                                                                                                                       
-    if ch == '0131':
-        return img/aunit 
-    if ch == '0171':
-        return img/aunit  
-    if ch == '0193':
-        return img/aunit 
-    if ch == '0211':
-        return img/aunit 
-    if ch == '0304':
-        return img/aunit 
-    if ch == '0335':
-        return img/aunit 
-    #If channel is not found, then just return original image
-    return img
+       
+def sdo_reverse_scale(img, ch, aunit=AUNIT, bunit=BUNIT)
+    """
+    Purpose: Given a scaled SDO image of a given channel, returned the unscaled image.
+    Currently a placeholder. scaling functions are subject to change based on EDA.
+    """
+    aunit= 1.0/aunit
+    bunit: 1.0/bunit
+    return sdo_scale(img, ch, aunit=aunit, bunit=bunit)
