15500225a0004ee4c7c278f79f01faf9499b7f67
vsalvatelli <vale.salvatelli@gmail.com>
2019-07-19 03:16:07 -0400

    Additions and bug fixes to data loader

--- src/sdo/sdo_dataset.py
+++ src/sdo/sdo_dataset.py
@@ -12,8 +12,6 @@ from sdo.pytorch_utilities import to_tensor
 from sdo.ds_utility import minmax_normalization
 import logging
 
-import pdb
-
 
 _logger = logging.getLogger(__name__)
 
@@ -22,7 +20,7 @@ class SDO_Dataset(Dataset):
     """ Custom Dataset class compatible with torch.utils.data.DataLoader. 
     It can be used to flexibly load a train or test dataset from the SDO local folder,
     asking for a specific range of years and a specific frequency in months, days, hours,
-    minutes. Normalization and bytescaling can be applied. """
+    minutes. Scaling is applied by default, normalization can be optionally applied. """
 
     def __init__(
         self,
@@ -32,15 +30,16 @@ class SDO_Dataset(Dataset):
         yr_range=[2010, 2018],
         mnt_step=1,
         day_step=1,
+        h_step=1,
         min_step=6,
         resolution=512,
         subsample=1,
         base_dir=BASEDIR,
         test=False,
         test_ratio=0.3,
-        shuffle=True,
-        normalization=1,
-        bytescaling=True,
+        shuffle=False,
+        normalization=0,
+        scaling=True,
         shuffle_seed=1234,
     ):
         """
@@ -53,19 +52,22 @@ class SDO_Dataset(Dataset):
             yr_range (list int): range of years to be selected
             mnt_step (int): month frequence
             day_step (int): day frequence
+            h_step (int): hour frequence
             min_step (int): minute frequence
             resolution (int): original resolution
             base_dir (str): path to the main dataset folder
             test (bool): if True, a test dataset is returned. By default the training dataset is returned.
             test_ratio (float): percentage of data to be used for testing. Training ratio is 1-test_ratio.
+            shuffle (bool): if True, the dataset will be shuffled. Keep it False if you want to return
+                            a time-ordered dataset.
             subsample (int): if 1 resolution of the final images will be as the original. 
                              If > 1 the image is downsampled. i.e. if resolution=512 and 
                              subsample=4, the images will be 128*128
-            shuffle (bool): if True, the dataset will be shuffled
             normalization (int): if 0 normalization is not applied, if >  0 a normalization
                                  by image is applied (only one type of normalization implemented 
                                  for now)
-            bytescaling (bool): if True pixel values are scaled for 8-bit display
+            scaling (bool): if True pixel values are scaled by the expected max value in active regions
+                            (see sdo.io.sdo_scale)
             shuffle_seed (int): seed to be used when shuffling the rows of the dataset. 
                                 if shuffle=False this is ignored
 
@@ -80,11 +82,12 @@ class SDO_Dataset(Dataset):
         self.yr_range = yr_range
         self.mnt_step = mnt_step
         self.day_step = day_step
+        self.h_step = h_step
         self.min_step = min_step
         self.test = test
         self.test_ratio = test_ratio
         self.normalization = normalization
-        self.bytescaling = bytescaling
+        self.scaling = scaling
         self.shuffle_seed = shuffle_seed
         self.files = self.create_list_files()
 
@@ -107,25 +110,32 @@ class SDO_Dataset(Dataset):
         """
         _logger.info('Loading SDOML from "%s"' % self.dir)
         files = []
+        discarded_tm = 0
         months = self.find_months()
-        for y in np.arange(self.yr_range[0], self.yr_range[1]):
-            for m in months:
+        for y in np.arange(self.yr_range[0], self.yr_range[1]+1):
+            for month in months:
                 for d in np.arange(1, 32, self.day_step):
-                    for h in np.arange(0, 24, self.min_step):
-                        # if a single channel is missing for the combination
-                        # of parameters result is -1
-                        result = sdo_find(y, m, d, h, 0,
-                                          instrs=self.instr,
-                                          channels=self.channels,
-                                          basedir=self.dir,
-                                          )
+                    for h in np.arange(0, 25, self.h_step):
+                        for minu in np.arange(0, 61, self.min_step):
+                            # if a single channel is missing for the combination
+                            # of parameters result is -1
+                            result = sdo_find(y, month, d, h, minu,
+                                              instrs=self.instr,
+                                              channels=self.channels,
+                                              basedir=self.dir,
+                                              )
                         if result != -1:
                             files.append(result)
+                        else:
+                            discarded_tm +=1
         if len(files) == 0:
             _logger.error("No input images found")
         else:
-            _logger.info("Number of SDO files = %d" % len(files))
+            _logger.info("Number of found timestamps = %d" % len(files))
+            _logger.info("Number of discarded timestamps = %d" % discarded_tm)
+            _logger.info("Number of SDO files = %d" % (len(files)*len(self.channels)))
             if self.shuffle:
+                _logger.warning("Shuffling is being applied, this will alter the time sequence.")
                 random.seed(self.shuffle_seed)
                 random.shuffle(files)
         return files
@@ -144,7 +154,7 @@ class SDO_Dataset(Dataset):
     def __getitem__(self, index):
         """
         This function will return a single row of the dataset, where each image has 
-        been normalized and bytescaled if requested in the class initialization.
+        been scaled and normalized if requested in the class initialization.
         Args:
             index (int): dataset row index
 
@@ -159,10 +169,10 @@ class SDO_Dataset(Dataset):
         for i in range(n_channels):
             img = np.load(self.files[index][i])["x"][
                 ::self.subsample, ::self.subsample]
+            if self.scaling:
+                item[:, :, i] = sdo_scale(img, self.channels[i])
             if self.normalization > 0:
                 img = self.normalize_by_img(img, self.normalization)
-            if self.bytescaling:
-                item[:, :, i] = sdo_bytescale(img, self.channels[i])
             else:
                 item[:, :, i] = img
         if n_channels == 1:
