42c9d8a7e7541b9d58e8d4cb106a9179c126a6bd
bradneuberg <bradneuberg@gmail.com>
2019-07-23 10:29:25 -0700

    Update global vars.

--- src/sdo/global_vars.py
+++ src/sdo/global_vars.py
@@ -1,6 +1,6 @@
-BASEDIR = '/gpfs/gpfs_gl4_16mb/b9p111/fdl_sw/SDOML'
+DATA_BASEDIR = '/gpfs/gpfs_gl4_16mb/b9p111/fdl_sw/SDOML'
 INVENTORY= '/gpfs/gpfs_gl4_16mb/b9p111/fdl_sw/SDOML/inventory.pkl'
-FILENAME_TEMPLATE = \
+DATA_FILENAME_TEMPLATE = \
 '{0:s}/{1:04d}/{2:02d}/{3:02d}/{4:s}{5:04d}{6:02d}{7:02d}_{8:02d}{9:02d}_{10:s}.npz'
 INITIAL_SIZE = 512
 B_CHANNELS = ['bx', 'by', 'bz']
