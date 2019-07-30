36b44143916f878b8532d9aae1306c38ef2750f0
Brad Neuberg <bradneuberg@gmail.com>
2019-07-30 19:29:18 +0000

    Several Refinements

--- src/sdo/models/autocalibration2.py
+++ src/sdo/models/autocalibration2.py
@@ -9,20 +9,22 @@ import torch.nn as nn
 
 _logger = logging.getLogger(__name__)
 
-# TODO: Put all the autocalibration model versions into a single file.
-class Autocalibration1(nn.Module):
-    def __init__(self, input_shape, output_dim):
+
+class Autocalibration2(nn.Module):
+    def __init__(self, input_shape, output_dim, increase_dim=2):
         super().__init__()
         if (len(input_shape) != 3):
             raise ValueError('Expecting an input_shape representing dimensions CxHxW')
         self._input_channels = input_shape[0]
         _logger.info('input_channels: {}'.format(self._input_channels))
-        self._conv2d1 = nn.Conv2d(in_channels=self._input_channels, out_channels=64, kernel_size=3)
-        self._conv2d2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3)
+        self._conv2d1 = nn.Conv2d(in_channels=self._input_channels, out_channels=64*increase_dim,
+                                  kernel_size=3)
+        self._conv2d2 = nn.Conv2d(in_channels=64*increase_dim, out_channels=128*increase_dim,
+                                  kernel_size=3)
         self._cnn_output_dim = self._cnn(torch.zeros(input_shape).unsqueeze(0)).nelement()
         _logger.info('cnn_output_dim: {}'.format(self._cnn_output_dim))
-        self._fc1 = nn.Linear(self._cnn_output_dim, 256)
-        self._fc2 = nn.Linear(256, output_dim)
+        self._fc1 = nn.Linear(self._cnn_output_dim, 256*increase_dim)
+        self._fc2 = nn.Linear(256*increase_dim, output_dim)
         
     def _cnn(self, x):
         x = self._conv2d1(x)
