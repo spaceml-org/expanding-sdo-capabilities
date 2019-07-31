69affc3eb3317c0c127ef51d12c3a58997761604
Brad Neuberg <bradneuberg@gmail.com>
2019-07-31 00:03:21 +0000

    Provide instructions and standard way to run jobs.
    The benefits of this is that we will get our training logs in a standard location, saved alongside checkpointed models and graphs.

--- src/sdo/pipelines/training_pipeline.py
+++ src/sdo/pipelines/training_pipeline.py
@@ -11,7 +11,7 @@ import numpy as np
 
 import torch
 
-from sdo.io import format_epoch
+from sdo.io import format_graph_prefix
 
 
 _logger = logging.getLogger(__name__)
@@ -95,43 +95,39 @@ class TrainingPipeline(object):
         pass
 
     def train(self, epoch, final_epoch=False):
-        _logger.info('\n\n')
-        _logger.info("===================================\n\n\n\n")
-        _logger.info("Training epoch {}\n".format(epoch))
-        self.model.train() # Indicate to PyTorch that we are in training mode.
-        losses = []
-        total_primary_metrics = []
-        times = []
-        for batch_idx, (input_data, gt_output, orig_data) in enumerate(self.train_loader):
-            with Timer() as t:
-                self.optimizer.zero_grad()
-                # Send the entire batch to the GPU as one to increase efficiency.
-                input_data = input_data.to(self.device)
-                gt_output = gt_output.to(self.device)
-                output = self.model(input_data)
-                loss = self.get_loss_func(output, gt_output)
-                loss.backward()
-                self.optimizer.step()
-                losses.append(float(loss))
+        with Timer() as epoch_perf:
+            _logger.info('\n\n')
+            _logger.info("===================================\n\n\n\n")
+            _logger.info("Training epoch {}\n".format(epoch))
+            self.model.train() # Indicate to PyTorch that we are in training mode.
+            losses = []
+            total_primary_metrics = []
+            for batch_idx, (input_data, gt_output, orig_data) in enumerate(self.train_loader):
+                with Timer() as iteration_perf:
+                    self.optimizer.zero_grad()
+                    # Send the entire batch to the GPU as one to increase efficiency.
+                    input_data = input_data.to(self.device)
+                    gt_output = gt_output.to(self.device)
+                    output = self.model(input_data)
+                    loss = self.get_loss_func(output, gt_output)
+                    loss.backward()
+                    self.optimizer.step()
+                    losses.append(float(loss))
 
-            times.append(t.elapsed)
-
-            if batch_idx % self.log_interval == 0:
-                primary_metric = self.calculate_primary_metric(epoch, output, gt_output)
-                total_primary_metrics.append(primary_metric)
-                self.print_epoch_details(epoch, batch_idx, self.batch_size_train,
-                                         self.train_dataset, loss, primary_metric,
-                                         t.elapsed, final_batch=False, train=True)
+                if batch_idx % self.log_interval == 0:
+                    primary_metric = self.calculate_primary_metric(epoch, output, gt_output)
+                    total_primary_metrics.append(primary_metric)
+                    self.print_epoch_details(epoch, batch_idx, self.batch_size_train,
+                                             self.train_dataset, loss, primary_metric,
+                                             iteration_perf.elapsed, final_batch=False,
+                                             train=True)
 
         # Generate extra metrics useful for debugging and analysis.
         self.generate_supporting_metrics(orig_data, output, input_data, gt_output, epoch,
-                                       train=True)
+                                         train=True)
 
-
-        # TODO: Have a timer for the entire batch loop rather than a mean for the total
-        # epoch time.
         self.print_epoch_details(epoch, batch_idx, self.batch_size_train, self.train_dataset,
-                                 np.mean(losses), primary_metric, np.mean(times),
+                                 np.mean(losses), primary_metric, epoch_perf.elapsed,
                                  final_batch=True, train=True)
 
         if epoch % self.save_interval == 0 or final_epoch:
@@ -140,41 +136,40 @@ class TrainingPipeline(object):
         return np.mean(losses), np.mean(total_primary_metrics)
 
     def test(self, epoch):
-        _logger.info("\n\nTesting epoch {}\n".format(epoch))
-        with torch.no_grad():
-            self.model.eval() # Indicate to PyTorch that we are in testing mode.
-            losses = []
-            total_primary_metrics = []
-            times = []
-            correct = 0
-            for batch_idx, (input_data, gt_output, orig_data) in enumerate(self.test_loader):
-                with Timer() as t:
-                    # Send the entire batch to the GPU as one to increase efficiency.
-                    input_data = input_data.to(self.device)
-                    gt_output = gt_output.to(self.device)
-                    output = self.model(input_data)
-                    output = output.to(self.device)
-                    loss = self.get_loss_func(output, gt_output)
-                    losses.append(float(loss))
+        with Timer() as epoch_perf:
+            _logger.info("\n\nTesting epoch {}\n".format(epoch))
+            with torch.no_grad():
+                self.model.eval() # Indicate to PyTorch that we are in testing mode.
+                losses = []
+                total_primary_metrics = []
+                times = []
+                correct = 0
+                for batch_idx, (input_data, gt_output, orig_data) in enumerate(self.test_loader):
+                    with Timer() as iteration_perf:
+                        # Send the entire batch to the GPU as one to increase efficiency.
+                        input_data = input_data.to(self.device)
+                        gt_output = gt_output.to(self.device)
+                        output = self.model(input_data)
+                        output = output.to(self.device)
+                        loss = self.get_loss_func(output, gt_output)
+                        losses.append(float(loss))
 
-                times.append(t.elapsed)
-
-                if batch_idx % self.log_interval == 0:
-                    primary_metric = self.calculate_primary_metric(epoch, output, gt_output)
-                    total_primary_metrics.append(primary_metric)
-                    self.print_epoch_details(epoch, batch_idx, self.batch_size_test,
-                                             self.test_dataset, loss, primary_metric,
-                                             t.elapsed, final_batch=False, train=False)
+                    if batch_idx % self.log_interval == 0:
+                        primary_metric = self.calculate_primary_metric(epoch, output, gt_output)
+                        total_primary_metrics.append(primary_metric)
+                        self.print_epoch_details(epoch, batch_idx, self.batch_size_test,
+                                                 self.test_dataset, loss, primary_metric,
+                                                 iteration_perf.elapsed, final_batch=False,
+                                                 train=False)
 
             # Generate extra metrics useful for debugging and analysis.
             self.generate_supporting_metrics(orig_data, output, input_data, gt_output, epoch,
-                                           train=False)
+                                             train=False)
 
-            # TODO: Have a timer for the entire batch loop rather than a mean for the total
-            # epoch time.
             self.print_epoch_details(epoch, batch_idx, self.batch_size_test,
                                      self.test_dataset, np.mean(losses), primary_metric,
-                                     t.elapsed, final_batch=True, train=False)
+                                     epoch_perf.elapsed, final_batch=True,
+                                     train=False)
 
             return np.mean(losses), np.mean(total_primary_metrics)
 
@@ -255,8 +250,6 @@ class TrainingPipeline(object):
             test_losses.append(loss)
             test_primary_metrics.append(primary_metric)
 
-            # TODO: For all the graphs, make sure the epoch number comes before the
-            # experiment name in the filename.
             # TODO: Put the experiment name into the title of the graph or as a
             # small subtitle somewhere.
             fig = plt.figure()
@@ -265,9 +258,8 @@ class TrainingPipeline(object):
             plt.xlabel('Epoch')
             plt.ylabel('Loss')
             plt.title('Training/testing loss after {} epochs'.format(epoch))
-            img_file = os.path.join(self.results_path, '{}_{}_loss_graph.png'.format(
-                self.exp_name,
-                format_epoch(epoch)))
+            img_file = os.path.join(self.results_path, '{}_loss_graph.png'.format(
+                format_graph_prefix(epoch, self.exp_name)))
             plt.legend()
             plt.savefig(img_file, bbox_inches='tight')
             plt.close()
@@ -280,9 +272,8 @@ class TrainingPipeline(object):
             plt.xlabel('Epoch')
             plt.ylabel('Primary metric')
             plt.title('Training/testing primary metric after {} epochs'.format(epoch))
-            img_file = os.path.join(self.results_path, '{}_{}_primary_metric_graph.png'.format(
-                self.exp_name,
-                format_epoch(epoch)))
+            img_file = os.path.join(self.results_path, '{}_primary_metric_graph.png'.format(
+                format_graph_prefix(epoch, self.exp_name)))
             plt.legend()
             plt.savefig(img_file, bbox_inches='tight')
             plt.close()
@@ -292,13 +283,13 @@ class TrainingPipeline(object):
         # TODO: Print the total aggregate training/testing time.
 
         # Print some final aggregate details at the complete end all epochs of training/testing.
-        _logger.info('\n\nFinal training loss after {} epochs: {:.6f}'.format(self.num_epochs, train_losses[-1]))
-        _logger.info('Final testing loss after {} epochs: {:.6f}'.format(self.num_epochs, test_losses[-1]))
+        _logger.info('\n\nFinal training loss after {} epochs: {}'.format(self.num_epochs, train_losses[-1]))
+        _logger.info('Final testing loss after {} epochs: {}'.format(self.num_epochs, test_losses[-1]))
 
         _logger.info('\nFinal best training loss: {}, encountered at epoch: {}'.format(
-            np.round(np.min(train_losses), decimals=4), np.array(train_losses).argmin() + 1))
+            np.min(train_losses), np.array(train_losses).argmin() + 1))
         _logger.info('Final best testing loss: {}, encountered at epoch: {}'.format(
-            np.round(np.min(test_losses), decimals=4), np.array(test_losses).argmin() + 1))
+            np.min(test_losses), np.array(test_losses).argmin() + 1))
 
         if self.is_higher_better_primary_metric():
             best = np.max
@@ -307,8 +298,7 @@ class TrainingPipeline(object):
             best = np.min
             best_arg = np.argmin
 
-        # TODO: Show more decimals here.
         _logger.info('\nFinal best training primary metric: {}, encountered at epoch: {}'.format(
-            np.round(best(train_primary_metrics), decimals=1), best_arg(train_primary_metrics) + 1))
+            best(train_primary_metrics), best_arg(train_primary_metrics) + 1))
         _logger.info('Final best testing primary metric: {}, encountered at epoch: {}'.format(
-            np.round(best(test_primary_metrics), decimals=1), best_arg(test_primary_metrics) + 1))
+            best(test_primary_metrics), best_arg(test_primary_metrics) + 1))
