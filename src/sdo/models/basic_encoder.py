688d6496158a41223f4ce94578b08505c6a7da2b
cheung@lmsal.com <b9p111ai@p10a139.pbm.ihost.com>
2019-08-06 05:49:10 -0400

    Added proxy_telescope_pipeline, which is essentially a copy of encoder_decoder_pipeline since the proxy telescope solution doesn't need to be an E/D. Also, calling it proxy telescope instead of virtual telescope

---
+++ src/sdo/models/basic_encoder.py
@@ -0,0 +1,114 @@
+from collections import namedtuple, defaultdict
+import math
+import random
+import os
+import shutil
+import torch
+import torchvision as tv
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import sys
+from functools import reduce
+import operator
+
+def prod(iterable):
+    return reduce(operator.mul, iterable, 1)
+
+class BasicEncoder(nn.Module):
+    """A Encoder Decoder module that is used to create one channel (e.g. AIA 211) from
+       three other AIA channels (e.g. AIA 94, 193, and 171).
+    """
+    
+    def __init__(self, input_shape=[3, 128, 128], hidden_dim=512):
+        """
+        Params: 
+        1. input_shape: of the form of (n_channels x width x height)
+        2. hidden_dim = number of hidden layers for the fully connected network.
+        """
+        super(BasicEncoder,self).__init__()
+        num_channels = input_shape[0]
+        self.conv1 = nn.Conv2d(in_channels=num_channels, out_channels=32, kernel_size=1, padding=0)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=1, padding=0)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, padding=0)
+        #self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3)
+        #self.conv5 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3)
+
+        #self.dconv5 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3)
+        #self.dconv4 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3)
+        #self.dconv3 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=3)
+        #self.dconv2 = nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=3)
+        #self.dconv1 = nn.ConvTranspose2d(in_channels=32, out_channels=1, kernel_size=3)
+        
+        #self.pool = nn.MaxPool2d(2, 2, return_indices=True)# in_channels, stride
+        #self.unpool = nn.MaxUnpool2d(2, 2)
+        
+        #sample_encoder_input = torch.zeros(input_shape)
+        #sample_encoder_output = self._encoder(sample_encoder_input.unsqueeze(0))[0]
+        #sample_encoder_output_dim = sample_encoder_output.nelement()
+        
+        #self.lin1 = nn.Linear(sample_encoder_output_dim, hidden_dim)
+        #self.lin2 = nn.Linear(hidden_dim, sample_encoder_output_dim)
+    
+        
+        print('EncoderDecoder architecture:')
+        print('Input shape: {}'.format(input_shape))
+        print('Input dim  : {}'.format(prod(input_shape)))
+        #print('Encoded dim: {}'.format(sample_encoder_output_dim))
+        #print('Hidden dim : {}'.format(hidden_dim))
+        print('Learnable params: {}'.format(sum([p.numel() for p in self.parameters()])))
+
+    def _encoder(self, x):
+        #print(x.shape)
+        x = self.conv1(x)
+        #print(x.shape)
+        x = F.relu(x)
+        x = self.conv2(x)
+        #print(x.shape)
+        x = F.relu(x)
+        x = self.conv3(x)
+        #print(x.shape)
+        x = F.relu(x)
+        
+        #x, indices1 = self.pool(x)
+        #x = self.conv3(x)
+        #x = F.relu(x)
+        #x, indices2 = self.pool(x)
+        #x = self.conv4(x)
+        #x = F.relu(x)
+        #x, indices3 = self.pool(x)
+        #x = self.conv5(x)
+        #x = F.relu(x)
+        #x, indices4 = self.pool(x) #
+        return x #, indices1, indices2, indices3, indices4
+
+    #def _decoder(self, x, indices1, indices2, indices3, indices4):
+    #    x = self.unpool(x, indices4) #
+    #    x =  self.dconv5(x)
+    #    x = F.relu(x)
+    #    x = self.unpool(x, indices3)
+    #    x =  self.dconv4(x)
+    #    x = F.relu(x)
+    #    x = self.unpool(x, indices2)
+    #    x =  self.dconv3(x)
+    #    x = F.relu(x)
+    #    x = self.unpool(x, indices1)
+    #    x = self.dconv2(x)
+    #    x = F.relu(x)
+    #    x = self.dconv1(x)
+    #    x = torch.relu(x)
+    #    return x
+
+    def forward(self,x):
+        batch_size = x.shape[0]
+        #x, indices1, indices2, indices3, indices4 = self._encoder(x)
+        x = self._encoder(x)
+        #Shap=x.shape
+        #x = x.view(batch_size, -1)
+        #x = self.lin1(x)
+        #x = self.lin2(x)
+        #x = F.relu(x)
+        #x = x.reshape(Shap)
+        #x = self._decoder(x, indices1, indices2, indices3, indices4)
+        return x
---
+++ src/sdo/pipelines/proxy_telescope_pipeline.py
@@ -0,0 +1,232 @@
+import logging
+import os
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import seaborn as sns
+
+from sklearn.metrics import mean_squared_error
+import scipy.stats as stats
+
+import pandas
+
+from sdo.datasets.proxy_telescope_sdo_dataset import ProxyTelescopeSDO_Dataset
+from sdo.io import format_graph_prefix
+from sdo.metrics.azimuth_metric import azimuthal_average, compute_2Dpsd
+from sdo.metrics.extended_encoder_decoder_metrics import structural_sim, pixel_sim
+from sdo.models.encoder_decoder import EncoderDecoder 
+from sdo.models.basic_encoder import BasicEncoder 
+from sdo.pipelines.training_pipeline import TrainingPipeline
+from sdo.pytorch_utilities import create_dataloader
+
+
+_logger = logging.getLogger(__name__)
+
+
+class ProxyTelescopePipeline(TrainingPipeline):
+    def __init__(self, exp_name, model_version, actual_resolution, scaled_height,
+                 scaled_width, device, instruments, wavelengths, subsample, batch_size_train,
+                 batch_size_test, test_ratio, log_interval, results_path, num_epochs, save_interval,
+                 additional_metrics_interval, continue_training, saved_model_path, saved_optimizer_path,
+                 start_epoch_at, yr_range, mnt_step, day_step, h_step, min_step, dataloader_workers,
+                 scaling, optimizer_weight_decay, optimizer_lr):
+        self.num_channels = len(wavelengths)
+
+        _logger.info('Using {} channels across the following wavelengths and instruments:'.format(
+            self.num_channels))
+        _logger.info('Wavelengths: {}'.format(wavelengths))
+        _logger.info('Instruments: {}'.format(instruments))
+
+        _logger.info('\nSetting up training dataset:')
+        train_dataset = ProxyTelescopeSDO_Dataset(
+                                    num_channels=self.num_channels,
+                                    instr=instruments,
+                                    channels=wavelengths, yr_range=yr_range,
+                                    mnt_step=mnt_step, day_step=day_step,
+                                    h_step=h_step, min_step=min_step,
+                                    resolution=actual_resolution,
+                                    subsample=subsample,
+                                    normalization=0, scaling=scaling,
+                                    test_ratio=test_ratio)
+
+        _logger.info('\nSetting up testing dataset:')
+        test_dataset = ProxyTelescopeSDO_Dataset(
+                                   num_channels=self.num_channels,
+                                   instr=instruments,
+                                   channels=wavelengths, yr_range=yr_range,
+                                   mnt_step=mnt_step, day_step=day_step,
+                                   h_step=h_step, min_step=min_step,
+                                   resolution=actual_resolution,
+                                   subsample=subsample,
+                                   normalization=0, scaling=scaling,
+                                   test_ratio=test_ratio, test=True)
+
+        train_loader = create_dataloader(train_dataset, batch_size_train,
+                                         dataloader_workers, train=True)
+        test_loader = create_dataloader(test_dataset, batch_size_test,
+                                        dataloader_workers, train=False)
+
+        if model_version == 1:
+            model = EncoderDecoder(input_shape=[self.num_channels - 1, scaled_height,
+                                                scaled_width])
+        if model_version == 2:
+            model = BasicEncoder(input_shape=[self.num_channels - 1, scaled_height,
+                                                scaled_width])
+            
+        else:
+            # Note: For other model_versions, simply instantiate whatever class
+            # you want to test your experiment for. You will have to update the code
+            # here to reference that class, preferably in sdo.models.*, such as
+            # sdo.models.Autocalibration2.
+            raise Exception('Unknown model version: {}'.format(model_version))
+
+        model.cuda(device)
+        optimizer = torch.optim.Adam(model.parameters(), weight_decay=optimizer_weight_decay,
+                                     lr=optimizer_lr)
+
+        super(ProxyTelescopePipeline, self).__init__(
+            exp_name=exp_name,
+            train_dataset=train_dataset,
+            test_dataset=test_dataset,
+            train_loader=train_loader,
+            test_loader=test_loader,
+            batch_size_train=batch_size_train,
+            batch_size_test=batch_size_test,
+            model=model,
+            optimizer=optimizer,
+            log_interval=log_interval,
+            results_path=results_path,
+            num_epochs=num_epochs,
+            device=device,
+            save_interval=save_interval,
+            additional_metrics_interval=additional_metrics_interval,
+            continue_training=continue_training,
+            saved_model_path=saved_model_path,
+            saved_optimizer_path=saved_optimizer_path,
+            start_epoch_at=start_epoch_at,
+            scaling=scaling)
+
+    def show_sample(self, loader):
+        """ Show some samples for debugging purposes before training/testing. """
+        input_data, gt_output, _ = loader.dataset[0]
+
+        # Print each of the input channels on their own line.
+        fig = plt.figure()
+        pos = 0
+        for i in range(self.num_channels - 1):
+            pos += 1
+
+            ax = fig.add_subplot(self.num_channels, 1, pos)
+            fig.subplots_adjust(top=3.0)
+            ax.set_title('Input Channel {}'.format(i + 1))
+            ax.axis('off')
+            ax.imshow(input_data[i].detach().numpy(), vmin=0, vmax=1)
+
+        pos += 1
+
+        ax = fig.add_subplot(self.num_channels, 1, pos)
+        fig.subplots_adjust(top=3.0)
+        ax.set_title('Output Channel {}'.format(self.num_channels - 1))
+        ax.axis('off')
+        ax.imshow(gt_output[0].detach().numpy(), vmin=0, vmax=1)
+
+        img_file = os.path.join(self.results_path, '{}_initial_sample.png'.format(
+            format_graph_prefix(0, self.exp_name)))
+        plt.savefig(img_file, bbox_inches='tight')
+        plt.close()
+        _logger.info('Initial sample saved to {}'.format(img_file))
+
+    def get_loss_func(self, output, gt_output):
+        """ Return the loss function this pipeline is using. """
+        distance = nn.MSELoss()
+        return torch.sqrt(distance(output, gt_output))
+
+    def calculate_primary_metric(self, epoch, output, gt_output):
+        """
+        Given some predicted output from a network and some ground truth, this method
+        calculates a scalar on how "well" we are doing for a given problem to gauge
+        progress during different experiments and during training. Note that we
+        already calculate and print out the loss outside of this method, so this
+        method is appropriate for other kinds of scalar values indicating progress
+        you'd like to use.
+        """
+        # TODO: Do this all on the GPU via Torch, rather than the CPU via Numpy.
+        prediction = output.detach().cpu().numpy()
+        ground_truth = gt_output.detach().cpu().numpy()
+
+        # TODO: Can also include the filtering components.
+        psd_1Dpred = azimuthal_average(compute_2Dpsd(prediction[0, 0, :, :]))
+        psd_1Dtruth = azimuthal_average(compute_2Dpsd(ground_truth[0, 0, :, :]))
+
+        primary_metric = mean_squared_error(psd_1Dtruth,psd_1Dpred) #MCMC removed log
+
+        # Note: lower values are better for our primary metric here.
+        return primary_metric
+
+    def is_higher_better_primary_metric(self):
+        # TODO: Confirm for the primary metric for the encoder/decoder above
+        # whether lower is better.
+        return False
+
+    def get_primary_metric_name(self):
+        return 'MSE of azimuthal average'
+
+    def generate_supporting_metrics(self, optional_debug_data, output, input_data, gt_output, epoch,
+                                    train):
+        """ Print debugging details on the final batch per epoch during training or testing. """
+        super(ProxyTelescopePipeline, self).generate_supporting_metrics(
+            optional_debug_data, output, input_data, gt_output, epoch, train)
+
+        input_data = input_data.detach().cpu().numpy()
+        output = output.detach().cpu().numpy()
+        gt_output = gt_output.detach().cpu().numpy()
+
+        # Print each of the input channels on their own line.
+        fig = plt.figure()
+        pos = 0
+        for i in range(self.num_channels - 1):
+            pos += 1
+
+            ax = fig.add_subplot(self.num_channels + 1, 1, pos)
+            fig.subplots_adjust(top=3.0)
+            ax.set_title('Input Channel {}'.format(i + 1))
+            ax.axis('off')
+            ax.imshow(input_data[0][i], vmin=0, vmax=1)
+
+        pos += 1
+
+        ax = fig.add_subplot(self.num_channels + 1, 1, pos)
+        fig.subplots_adjust(top=3.0)
+        ax.set_title('Predicted Output Channel {}'.format(self.num_channels - 1))
+        ax.axis('off')
+        ax.imshow(output[0][0], vmin=0, vmax=1)
+
+        pos += 1
+
+        ax = fig.add_subplot(self.num_channels + 1, 1, pos)
+        fig.subplots_adjust(top=3.0)
+        ax.set_title('Ground Truth Output Channel {}'.format(self.num_channels - 1))
+        ax.axis('off')
+        ax.imshow(gt_output[0][0], vmin=0, vmax=1)
+
+        img_file = os.path.join(self.results_path, '{}_debug_sample.png'.format(
+            format_graph_prefix(epoch, self.exp_name)))
+        plt.savefig(img_file, bbox_inches='tight')
+        plt.close()
+        _logger.info('Debug sample saved to {}'.format(img_file))
+
+        # Get the similarity values between the predicted and ground truth outputs.
+        # TODO: Do all these operations on the GPU with Torch, not the CPU via Numpy.
+        struc_sim = structural_sim(output[0][0], gt_output[0][0])
+        pix_sim = pixel_sim(output[0][0], gt_output[0][0])
+        #   sift_sim = sift_sim(output[0][0], gt_output[0][0])
+        #  emd = earth_movers_distance(output[0][0], gt_output[0][0])
+        _logger.info('Structural similarity for single sample: {}'.format(struc_sim))
+        _logger.info('Pixel similarity for single sample: {}'.format(pix_sim))
+        
