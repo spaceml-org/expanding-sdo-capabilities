d82f00f3d535f37d15b889abb1a504f38c58e402
vsalvatelli <vale.salvatelli@gmail.com>
2019-08-11 16:34:35 -0400

    Removed batch normalization

--- src/sdo/models/unet_basic_blocks.py
+++ src/sdo/models/unet_basic_blocks.py
@@ -12,7 +12,7 @@ from torch.autograd import Variable
 def conv_block(in_dim,out_dim,act_fn):
     model = nn.Sequential(
         nn.Conv2d(in_dim,out_dim, kernel_size=3, stride=1, padding=1),
-        nn.BatchNorm2d(out_dim),
+        #nn.BatchNorm2d(out_dim),
         act_fn,
     )
     return model
@@ -21,7 +21,7 @@ def conv_block(in_dim,out_dim,act_fn):
 def conv_trans_block(in_dim,out_dim,act_fn):
     model = nn.Sequential(
         nn.ConvTranspose2d(in_dim,out_dim, kernel_size=3, stride=2, padding=1,output_padding=1),
-        nn.BatchNorm2d(out_dim),
+        #nn.BatchNorm2d(out_dim),
         act_fn,
     )
     return model
@@ -36,7 +36,7 @@ def conv_block_2(in_dim,out_dim,act_fn):
     model = nn.Sequential(
         conv_block(in_dim,out_dim,act_fn),
         nn.Conv2d(out_dim,out_dim, kernel_size=3, stride=1, padding=1),
-        nn.BatchNorm2d(out_dim),
+        #nn.BatchNorm2d(out_dim),
     )
     return model    
 
@@ -46,6 +46,6 @@ def conv_block_3(in_dim,out_dim,act_fn):
         conv_block(in_dim,out_dim,act_fn),
         conv_block(out_dim,out_dim,act_fn),
         nn.Conv2d(out_dim,out_dim, kernel_size=3, stride=1, padding=1),
-        nn.BatchNorm2d(out_dim),
+        #nn.BatchNorm2d(out_dim),
     )
     return model
