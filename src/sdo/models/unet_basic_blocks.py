b3f07c52359d210faf3e84168f7d82b01107b831
vsalvatelli <vale.salvatelli@gmail.com>
2019-08-23 03:39:15 -0400

    Remove commented BatchNorm

--- src/sdo/models/unet_basic_blocks.py
+++ src/sdo/models/unet_basic_blocks.py
@@ -21,7 +21,6 @@ def conv_block(in_dim,out_dim,act_fn):
 def conv_trans_block(in_dim,out_dim,act_fn):
     model = nn.Sequential(
         nn.ConvTranspose2d(in_dim,out_dim, kernel_size=3, stride=2, padding=1,output_padding=1),
-        #nn.BatchNorm2d(out_dim),
         act_fn,
     )
     return model
@@ -36,7 +35,6 @@ def conv_block_2(in_dim,out_dim,act_fn):
     model = nn.Sequential(
         conv_block(in_dim,out_dim,act_fn),
         nn.Conv2d(out_dim,out_dim, kernel_size=3, stride=1, padding=1),
-        #nn.BatchNorm2d(out_dim),
     )
     return model    
 
@@ -46,6 +44,5 @@ def conv_block_3(in_dim,out_dim,act_fn):
         conv_block(in_dim,out_dim,act_fn),
         conv_block(out_dim,out_dim,act_fn),
         nn.Conv2d(out_dim,out_dim, kernel_size=3, stride=1, padding=1),
-        #nn.BatchNorm2d(out_dim),
     )
     return model
