f40d4fa6ab28402cfe02b85364688d8bdd6562f3
bradneuberg <bradneuberg@gmail.com>
2020-01-15 21:34:40 -0800

    Use *.npz files, not memory mapped ones.

--- src/sdo/global_vars.py
+++ src/sdo/global_vars.py
@@ -1,6 +1,6 @@
 DATA_BASEDIR = '/fdl_sdo_data/SDOMLmm'
 DATA_INVENTORY= '/fdl_sdo_data/SDOMLmm/inventory.pkl'
 DATA_FILENAME_TEMPLATE = \
-'{0:s}/{1:04d}/{2:02d}/{3:02d}/{4:s}{5:04d}{6:02d}{7:02d}_{8:02d}{9:02d}_{10:s}.mm'
+'{0:s}/{1:04d}/{2:02d}/{3:02d}/{4:s}{5:04d}{6:02d}{7:02d}_{8:02d}{9:02d}_{10:s}.npz'
 B_CHANNELS = ['bx', 'by', 'bz']
 UV_CHANNELS = ['1600', '1700', '0094', '0131', '0171', '0193', '0211', '0304', '0335'] 
