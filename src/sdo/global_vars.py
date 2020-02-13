73ede89c5b42d0ed654dc0d7892f1abea4595acb
bradneuberg <bradneuberg@gmail.com>
2020-02-13 13:37:18 -0800

    Future work.

--- src/sdo/global_vars.py
+++ src/sdo/global_vars.py
@@ -1,5 +1,6 @@
-DATA_BASEDIR = '/fdl_sdo_data/SDOMLmm'
-DATA_INVENTORY= '/fdl_sdo_data/SDOMLmm/inventory.pkl'
+# TODO!!! Root these two variables out, or change their names to reflect their GCP usage.
+DATA_BASEDIR = '/fdl_sdo_data/SDOMLnpz'
+DATA_INVENTORY= '/fdl_sdo_data/SDOMLnpz/inventory.pkl'
 DATA_FILENAME_TEMPLATE = \
 '{0:s}/{1:04d}/{2:02d}/{3:02d}/{4:s}{5:04d}{6:02d}{7:02d}_{8:02d}{9:02d}_{10:s}.npz'
 B_CHANNELS = ['bx', 'by', 'bz']
