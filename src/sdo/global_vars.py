d755455f439ba1c1964ce0dca8e2bde290a288e2
Valentina Salvatelli <vale.salvatelli@gmail.com>
2020-06-18 22:21:18 +0000

    Implementation of Heteroscedastic regression for autocal

--- src/sdo/global_vars.py
+++ src/sdo/global_vars.py
@@ -1,5 +1,5 @@
 DATA_BASEDIR = '/fdl_sdo_data/SDOMLmm/fdl_sw/SDOMLmm'
-DATA_INVENTORY= '/fdl_sdo_data/SDOMLmm/fdl_sw/SDOMLmm/inventory.pkl'
+DATA_INVENTORY= '/fdl_sdo_data/SDOMLmm/fdl_sw/SDOMLmm/inventory_1904.pkl'
 DATA_FILENAME_TEMPLATE = \
 '{0:s}/{1:04d}/{2:02d}/{3:02d}/{4:s}{5:04d}{6:02d}{7:02d}_{8:02d}{9:02d}_{10:s}.mm'
 B_CHANNELS = ['bx', 'by', 'bz']
