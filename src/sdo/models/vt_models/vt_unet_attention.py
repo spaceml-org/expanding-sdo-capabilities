53ea9f8f817036b5d467e8c6311c1c962fe031f5
vale-salvatelli <vale.salvatelli@gmail.com>
2021-01-16 20:03:07 +0000

    all implemented

--- src/sdo/models/vt_models/vt_unet_attention.py
+++ src/sdo/models/vt_models/vt_unet_attention.py
@@ -1,8 +1,7 @@
 import logging
 import torch
 import torch.nn as nn
-from sdo.models.vt_models.vt_unet_basic_blocks import (maxpool, conv_block_2,  conv_block_2_sym,
-                                                       conv_trans_block, up_conv, Attention_block)
+from sdo.models.vt_models.vt_unet_basic_blocks import (maxpool,  conv_block_2_sym, up_conv, Attention_block)
 
 _logger = logging.getLogger(__name__)
 
@@ -12,6 +11,7 @@ _logger = logging.getLogger(__name__)
 class UNet_Attention(nn.Module):
     def __init__(self,  input_shape=[3, 128, 128], num_filter=64,
                  LR_neg_slope=0.2, depth=4):
+        # the depth corresponds to the number of convolution blocks applied
         super(UNet_Attention, self).__init__()
         self.in_dim = input_shape[0]
         self.out_dim = 1
@@ -26,11 +26,12 @@ class UNet_Attention(nn.Module):
         alpha = [1, 2]
         for i in range(1, self.depth):
             down = conv_block_2_sym(self.num_filter * alpha[0], self.num_filter * alpha[1], act_fn)
-            # the self.pool list could be eliminated given each element is the same
             pool = maxpool()
             self.down.append(down)
             self.pool.append(pool)
             alpha = [k * 2 for k in alpha]
+        self.bridge = conv_block_2_sym(self.num_filter * alpha[0], self.num_filter * alpha[1], act_fn)
+        # expansion phase
         self.up = []
         self.att = []
         self.up_conv = []
@@ -56,7 +57,6 @@ class UNet_Attention(nn.Module):
             nn.ReLU(),
         )
 
-
     def forward(self, data_input):
         down = []
         pool = []
@@ -67,12 +67,18 @@ class UNet_Attention(nn.Module):
             down.append(self.down[i](pool[i - 1]))
             pool.append(self.pool[i](down[i]))
 
-        up_conv =  # handle bridge here
+        bridge = self.bridge(pool[self.depth - 1])
+        up = self.up[0](bridge)
+        att = self.att[0](down[self.depth - 1], up)
+        concat = torch.cat([att, down[self.depth - 1]], dim=1)
+        up_conv = self.up[0](concat)
+        down_i = self.depth - 2
         for i in range(1, self.depth):
             up = self.up[i](up_conv)
-            att = self.att[i](down[i-1], up)
+            att = self.att[i](down[down_i], up)
             concat = torch.cat([up, att], dim=1)
             up_conv = self.up_conv[i](concat)
+            down_i = down_i - 1
 
         out = self.out(up_conv)
         return out
