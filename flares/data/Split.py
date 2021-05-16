1ef9217ff5609f699c9c4a24d4197dec2c417cf7
Valentina Salvatelli <vale.salvatelli@gmail.com>
2021-05-16 12:16:15 +0000

    Vt flares

---
+++ flares/data/Split.py
@@ -0,0 +1,13 @@
+"""
+Quick script to split all the available flares events in modelling and holdout datasets
+"""
+import pandas as pd
+
+filename = '/home/Valentina/expanding-sdo-capabilities/rare_events/flares_ordered.csv'
+df = pd.read_csv(filename)
+df = df.sample(frac=1).reset_index(drop=True)
+holdout_filename = '/home/Valentina/expanding-sdo-capabilities/rare_events/flares_holdout.csv'
+train_filename = '/home/Valentina/expanding-sdo-capabilities/rare_events/flares_modelling.csv'
+holdout_size = int(df.shape[0]*0.30)
+df[:holdout_size].to_csv(holdout_filename, index=False)
+df[holdout_size:].to_csv(train_filename, index=False)
